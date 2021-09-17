83a6d8eac33e8877575ff01f86ce590af0758f5f
Nicolas Tito <mrnicolastito@icloud.com>
2021-09-17 19:30:17 +0100

    ..

--- src/main.c
+++ src/main.c
@@ -6,14 +6,20 @@
 /*   By: nide-mel <nide-mel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 16:57:57 by nide-mel          #+#    #+#             */
-/*   Updated: 2021/09/15 17:12:42 by nide-mel         ###   ########.fr       */
+/*   Updated: 2021/09/15 17:26:26 by nide-mel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main()
+int	main(int ac, char **av)
 {
-	printf("HELLO");
+	if (ac != 2)
+	{
+		ft_putstr_fd("ARGUMENTS ERROR 😐", 1);
+		return (-1);
+	}
+	if(!av[1])
+		return (0);
 	return (0);
 }
